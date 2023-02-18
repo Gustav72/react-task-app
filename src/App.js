c0cd2bc00904e6e2d8e5f028928a4f82aa74b376
Gustav72 <wolfgangkirstein7@gmail.com>
2023-02-18 13:22:33 -0500

    adjust dir name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import uniqid from 'uniqid';
-import Overview from './components/Overview';
+import Overview from './component/Overview';
 
 class App extends Component {
   
